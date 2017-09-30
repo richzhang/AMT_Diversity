e04120128bdfe1cf66e61c0a76d2af760a076919
Richard Zhang <rzhang88@gmail.com>
2017-09-30 16:28:01 -0700

    perceptual

--- getOpts.m
+++ getOpts.m
@@ -4,18 +4,57 @@ function [opt] = getOpts(expt_name)
         
 		case 'example_expt'
 			opt = getDefaultOpts();
-		
 			opt.which_algs_paths = {'my_alg','baseline_alg'};
 			opt.Nimgs = 1000;
-			opt.ut_id = 'unset'; % set this using http://uniqueturker.myleott.com/
+			opt.ut_id = '94149d445af4a3af8dd5d41614353c0b'; % set this using http://uniqueturker.myleott.com/
 			opt.base_url = 'https://www.mywebsite.com/example_expt_data/';
 			opt.instructions_file = './instructions_basic.html';
 			opt.short_instructions_file = './short_instructions_basic.html';
 			opt.consent_file = './consent_basic.html';
 			opt.use_vigilance = false;
 			opt.paired = true;
-		
-		otherwise
+
+		case 'perceptual'
+			opt = getDefaultOpts();
+			opt.which_algs_paths = {'p0','p1'};
+            opt.gt_path = 'ref';
+			opt.Nimgs = 100;
+			opt.ut_id = '94149d445af4a3af8dd5d41614353c0b'; % set this using http://uniqueturker.myleott.com/
+			opt.base_url = 'https://eecs.berkeley.edu/~rich.zhang/research/2017_07_perceptual/easy_collect_richard/';
+			opt.instructions_file = './instructions_basic.html';
+			opt.short_instructions_file = './short_instructions_basic.html';
+			opt.consent_file = './consent_basic.html';
+			opt.use_vigilance = false;
+			opt.paired = true;
+		            
+		case 'perceptual_mod'
+			opt = getDefaultOpts();
+			opt.which_algs_paths = {'p0','ref','p1'};
+%             opt.gt_path = 'ref';
+            opt.Nimgs = 2000;                        % number of images to test
+            opt.Npairs = 105;                        % number of paired comparisons per HIT
+            opt.Npractice = 5;                     % number of practice trials per HIT (number of non-practice trials is opt.Npairs-opt.Npractice)
+            opt.Nhits_per_alg = 100;                 % number of HITs
+
+            opt.ut_id = '94149d445af4a3af8dd5d41614353c0b'; % set this using http://uniqueturker.myleott.com/
+% 			opt.base_url = 'https://eecs.berkeley.edu/~rich.zhang/research/2017_07_perceptual/easy_collect_richard/';
+% 			opt.base_url = 'https://eecs.berkeley.edu/~rich.zhang/research/2017_07_perceptual/easy_collect_richard_adobe_32/';
+			opt.base_url = 'https://eecs.berkeley.edu/~rich.zhang/research/2017_07_perceptual/easy_collect_richard_adobe_32_4/';
+			opt.instructions_file = './instructions_basic_mod.html';
+			opt.short_instructions_file = './short_instructions_basic.html';
+			opt.consent_file = './consent_basic_mod.html';
+
+            opt.use_vigilance = true;
+			opt.vigilance_path = 'vigilance';
+            opt.Nvigilance = 1000;                  % number of vigilance images available
+        	opt.vigilance_freq = .1;               % percent of trials that are vigilance tests
+            
+            opt.im_height = 32;                    % dimensions at which to display the stimuli
+            opt.im_width = 32;                     %
+            
+            opt.paired = true;
+		            
+        otherwise
 			error(sprintf('no opts defined for experiment %s',expt_name));
 	end
 	
--- mk_expt.m
+++ mk_expt.m
@@ -1,113 +1,137 @@
 function [] = mk_expt(expt_name)
-	
-	%% expt parameters
-	opt = getOpts(expt_name);
-	
-	%% check parameters
-	checkOpts(opt);
-	
-	%% make dir for expt, overwriting if it already exists
-	if (exist(opt.expt_name,'dir'))
-		system(sprintf('rm -r ./%s',opt.expt_name));
-	end
-	mkdir(opt.expt_name);
-	
-	%%
-	rng('shuffle');
-	
-	%% 
-	csv_fname = fullfile(opt.expt_name,sprintf('expt_input_data.csv'));
-	
-	gt_side = {};
-	images_left = {};
-	images_right = {};
-	
-	for i=1:opt.Npairs
-	    gt_side{1,i} = sprintf('gt_side%d',i);
-	    images_left{1,i} = sprintf('images_left%d',i);
-	    images_right{1,i} = sprintf('images_right%d',i);
-	end
-	
-	for which_alg_curr=1:length(opt.which_algs_paths) % each trial tests just one alg
-	    
-	    for j=1:opt.Nhits_per_alg % same number of HITs for each alg
-	        
-	        which_imgs_alg = randperm(opt.Nimgs); which_imgs_alg = which_imgs_alg(1:opt.Npairs); % choose random set of images to test on, from test set
-			which_imgs_gt = [];
-			if (opt.paired)
-				which_imgs_gt = which_imgs_alg;
-			else
-				which_imgs_gt = randperm(opt.Nimgs); which_imgs_gt = which_imgs_gt(1:opt.Npairs); % choose random set of images to test on, from test set
-			end
-			
-	        for i=1:opt.Npairs
-	            
-				
-	            if (opt.use_vigilance && (rand(1)<=opt.vigilance_freq)) % opt.vigilance_freq proportion of trials are vigilance checks
-	                alg_im_name = sprintf('%s/%d',opt.vigilance_path,which_imgs_alg(i));
-	            else
-	                alg_im_name = sprintf('%s/%d',opt.which_algs_paths{which_alg_curr},which_imgs_alg(i));
-	            end
-	            
-	            if (randi(2)==1) % randomize which side of UI gt is on
-	                gt_side{(which_alg_curr-1)*opt.Nhits_per_alg+j+1,i} = 'left';
-	                images_left{(which_alg_curr-1)*opt.Nhits_per_alg+j+1,i} = sprintf('%s/%d',opt.gt_path,which_imgs_gt(i));
-	                images_right{(which_alg_curr-1)*opt.Nhits_per_alg+j+1,i} = alg_im_name;
-	            else
-	                gt_side{(which_alg_curr-1)*opt.Nhits_per_alg+j+1,i} = 'right';
-	                images_left{(which_alg_curr-1)*opt.Nhits_per_alg+j+1,i} = alg_im_name;
-	                images_right{(which_alg_curr-1)*opt.Nhits_per_alg+j+1,i} = sprintf('%s/%d',opt.gt_path,which_imgs_gt(i));
-	            end
-	        end
-	    end
-	end
-	
-	A = cat(2,gt_side,images_left,images_right);
-	
-	rp = randperm(size(A,1)-1)+1; A = A([1,rp],:); % randomize HIT order (I think MTurk does this but let's do it here as well for safety)
-	
-	fid = fopen(csv_fname,'w');
-	for i=1:size(A,1)
-	    for j=1:size(A,2)-1
-	       fprintf(fid,[A{i,j},',']);
-	    end
-	    fprintf(fid,A{i,end});
-	    fprintf(fid,'\n');
-	end
-	fclose(fid);
-	
-	
-	%% html code generator
-	html = fileread('index_template.html');
-	
-	html = strrep(html, '{{UT_ID}}', opt.ut_id);
-	html = strrep(html, '{{BASE_URL}}', opt.base_url);
-	
-	html = strrep(html, '{{INSTRUCTIONS}}', fileread(opt.instructions_file));
-	html = strrep(html, '{{SHORT_INSTRUCTIONS}}', fileread(opt.short_instructions_file));
-	html = strrep(html, '{{CONSENT}}', fileread(opt.consent_file));
-	
-	html = strrep(html, '{{IM_DIV_HEIGHT}}', num2str(opt.im_height+2));
-	html = strrep(html, '{{IM_DIV_WIDTH}}', num2str(opt.im_width+2));
-	html = strrep(html, '{{IM_HEIGHT}}', num2str(opt.im_height));
-	html = strrep(html, '{{IM_WIDTH}}', num2str(opt.im_width));
-    
-    html = strrep(html, '{{N_PRACTICE}}', num2str(opt.Npractice));
-    html = strrep(html, '{{TOTAL_NUM_IMS}}', num2str(opt.Npairs));
-	
-	s = [];
-	for i=1:opt.Npairs
-	    s = cat(2,s,sprintf('sequence_helper("${gt_side%d}","${images_left%d}","${images_right%d}");\n',i,i,i));
-	end
-	html = strrep(html, '{{SEQUENCE}}', s);
-	
-	s = [];
-	for i=1:opt.Npairs
-	    s = cat(2,s,sprintf('<input type="hidden" name="selection%d" id="selection%d" value="unset">\n',i,i));
-	end
-	html = strrep(html, '{{SELECTION}}', s);
-	
-	fid = fopen(fullfile(opt.expt_name,'index.html'),'w');
-	fprintf(fid,'%s',html);
-	fclose(fid);
+
+%% expt parameters
+opt = getOpts(expt_name);
+
+%% check parameters
+checkOpts(opt);
+
+%% make dir for expt, overwriting if it already exists
+if (exist(opt.expt_name,'dir'))
+    system(sprintf('rm -r ./%s',opt.expt_name));
 end
+mkdir(opt.expt_name);
+
+%%
+rng('shuffle');
+
+%%
+csv_fname = fullfile(opt.expt_name,sprintf('expt_input_data.csv'));
+
+images_p0 = {};
+images_ref = {};
+images_p1 = {};
+
+for i=1:opt.Npairs
+    images_p0{1,i} = sprintf('images_p0_%i',i);
+    images_ref{1,i} = sprintf('images_ref_%i',i);
+    images_p1{1,i} = sprintf('images_p1_%i',i);
+end
+
+% for which_alg_curr=1:length(opt.which_algs_paths) % each trial tests just one alg
+for which_alg_curr=1
+    for j=1:opt.Nhits_per_alg % number of hits
+        
+        % vigilance indices
+        vigilance_inds = zeros(1,opt.Npairs);
+        Nvigil_practice = ceil(opt.vigilance_freq*opt.Npractice);
+        Nvigil_real = ceil(opt.vigilance_freq*(opt.Npairs-opt.Npractice));
+        Nvigil_total = Nvigil_practice + Nvigil_real;
+        if(opt.use_vigilance)
+            which_imgs_vig = randperm(opt.Nvigilance)-1;
+            which_imgs_vig = which_imgs_vig(1:Nvigil_total); % test images
+
+            vigil_inds_practice = randperm(opt.Npractice);
+            vigil_inds_practice = vigil_inds_practice(1:Nvigil_practice);
+            vigilance_inds(vigil_inds_practice) = which_imgs_vig(1:Nvigil_practice); % put test image indices into vigilance
+            
+            vigil_inds_real = randperm(opt.Npairs-opt.Npractice);
+            vigil_inds_real = vigil_inds_real(1:Nvigil_real)+opt.Npractice;
+            vigilance_inds(vigil_inds_real) = which_imgs_vig(Nvigil_practice+1:end); % put test image indices into vigilance
+        end
+        
+        % actual test images
+        which_imgs_alg = randperm(opt.Nimgs)-1;
+        which_imgs_alg = which_imgs_alg(1:opt.Npairs); % choose random set of images to test on, from test set
+
+        for i=1:opt.Npairs
+%             if (opt.use_vigilance && (rand(1)<=opt.vigilance_freq)) % opt.vigilance_freq proportion of trials are vigilance checks
+%                 alg_im_name = sprintf('%s/%06d',opt.vigilance_path,which_imgs_alg(i));
+%             else
+%                 alg_im_name = sprintf('%s/%06d',opt.which_algs_paths{which_alg_curr},which_imgs_alg(i));
+%             end
+            
+            if(vigilance_inds(i))
+                images_p0{(which_alg_curr-1)*opt.Nhits_per_alg+j+1,i} = sprintf('%s/%s/%06d',opt.vigilance_path,'p0',vigilance_inds(i));
+                images_ref{(which_alg_curr-1)*opt.Nhits_per_alg+j+1,i} = sprintf('%s/%s/%06d',opt.vigilance_path,'ref',vigilance_inds(i));
+                images_p1{(which_alg_curr-1)*opt.Nhits_per_alg+j+1,i} = sprintf('%s/%s/%06d',opt.vigilance_path,'p1',vigilance_inds(i));
+            else
+                images_p0{(which_alg_curr-1)*opt.Nhits_per_alg+j+1,i} = sprintf('%s/%06d','p0',which_imgs_alg(i));
+                images_ref{(which_alg_curr-1)*opt.Nhits_per_alg+j+1,i} = sprintf('%s/%06d','ref',which_imgs_alg(i));
+                images_p1{(which_alg_curr-1)*opt.Nhits_per_alg+j+1,i} = sprintf('%s/%06d','p1',which_imgs_alg(i));
+            end
+            
+            % 	            if (randi(2)==1) % randomize which side of UI gt is on
+            % 	                gt_side{(which_alg_curr-1)*opt.Nhits_per_alg+j+1,i} = 'left';
+            %  	                images_p0{(which_alg_curr-1)*opt.Nhits_per_alg+j+1,i} = sprintf('%s/%d',opt.gt_path,which_imgs_gt(i));
+            % 	            else
+            % 	                gt_side{(which_alg_curr-1)*opt.Nhits_per_alg+j+1,i} = 'right';
+            % 	                images_p0{(which_alg_curr-1)*opt.Nhits_per_alg+j+1,i} = alg_im_name;
+            %  	                images_p1{(which_alg_curr-1)*opt.Nhits_per_alg+j+1,i} = sprintf('%s/%d',opt.gt_path,which_imgs_gt(i));
+            % 	                images_p1{(which_alg_curr-1)*opt.Nhits_per_alg+j+1,i} = sprintf('%s/%06d',opt.gt_path,which_imgs_gt(i));
+            % 	            end
+        end
+    end
+end
+
+% 	A = cat(2,gt_side,images_p0,images_p1);
+A = cat(2,images_p0,images_ref,images_p1);
+
+rp = randperm(size(A,1)-1)+1; A = A([1,rp],:); % randomize HIT order (I think MTurk does this but let's do it here as well for safety)
+
+fid = fopen(csv_fname,'w');
+for i=1:size(A,1)
+    for j=1:size(A,2)-1
+        fprintf(fid,[A{i,j},',']);
+    end
+    fprintf(fid,A{i,end});
+    fprintf(fid,'\n');
+end
+fclose(fid);
+
+
+%% html code generator
+html = fileread('index_template_mod.html');
+
+html = strrep(html, '{{UT_ID}}', opt.ut_id);
+html = strrep(html, '{{BASE_URL}}', opt.base_url);
+
+html = strrep(html, '{{INSTRUCTIONS}}', fileread(opt.instructions_file));
+html = strrep(html, '{{SHORT_INSTRUCTIONS}}', fileread(opt.short_instructions_file));
+html = strrep(html, '{{CONSENT}}', fileread(opt.consent_file));
+
+html = strrep(html, '{{IM_DIV_HEIGHT}}', num2str(opt.im_height+2));
+html = strrep(html, '{{IM_DIV_WIDTH}}', num2str(opt.im_width+2));
+html = strrep(html, '{{IM_HEIGHT}}', num2str(opt.im_height));
+html = strrep(html, '{{IM_WIDTH}}', num2str(opt.im_width));
+
+html = strrep(html, '{{N_PRACTICE}}', num2str(opt.Npractice));
+html = strrep(html, '{{TOTAL_NUM_IMS}}', num2str(opt.Npairs));
+
+s = [];
+for i=1:opt.Npairs
+    s = cat(2,s,sprintf('sequence_helper("${images_p0_%i}","${images_ref_%i}","${images_p1_%i}");\n',i,i,i));
+end
+html = strrep(html, '{{SEQUENCE}}', s);
+
+s = [];
+for i=1:opt.Npairs
+    s = cat(2,s,sprintf('<input type="hidden" name="selection%i" id="selection%i" value="unset">\n',i,i));
+end
+html = strrep(html, '{{SELECTION}}', s);
+
+fid = fopen(fullfile(opt.expt_name,'index.html'),'w');
+fprintf(fid,'%s',html);
+fclose(fid);
+end
+
